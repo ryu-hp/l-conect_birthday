7d6ee80a9a93a146027395bb15eab1f3ea8d979e
ryu-hp <ryupon34689@gmail.com>
2025-12-09 02:47:15 +0900

    スタイルの調整とバースデーページの新規作成、コンテナ幅とパディングの変更を含む

---
+++ page-birthday.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>安家社長バースデイ＆L・コネクト5周年パーティーのお知らせ</title>
+</head>
+<body>
+  <header id="birth-header" class="birth-header">
+    <div class="birth-header__container container">
+      <div class="birth-header__text">
+        <img src="" alt="">
+      </div>
+    </div>
+  </header>
+  
+  <main id="birth-main" class="birth-main">
+    <section id = "birth-fv" class = "birth-fv birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-fv__inner container">
+          <div class="birth-fv__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+        <div class="birth-fv__ceo-image">
+          <img src="" alt="">
+        </div>
+      </div>
+    </section>
+    <section id = "birth-key-message" class = "birth-key-message birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-key-message__inner container">
+          <div class="birth-key-message__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id = "birth-head" class = "birth-head birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-head__inner container">
+          <div class="birth-head__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id = "birth-description" class = "birth-description birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-description__inner container">
+          <div class="birth-description__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id = "birth-ribbon" class = "birth-ribbon birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-ribbon__inner container">
+          <div class="birth-ribbon__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id = "birth-info" class = "birth-info birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-info__inner container">
+          <div class="birth-info__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id = "birth-form" class = "birth-form birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-form__inner container">
+          <div class="birth-form__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id = "birth-footer" class = "birth-footer birth-section">
+      <div class="birth-wrapper">
+        <div class="birth-footer__inner container">
+          <div class="birth-footer__contentes">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</body>
+</html>
